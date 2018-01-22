ee24112eedef685448ae053d69453b22f4b868c2
mattNat <mattsanpedro76@gmail.com>
2018-01-22 08:59:28 -0800

    add Item.js store.js and move store variable

---
+++ scripts/store.js
@@ -0,0 +1,39 @@
+const store = (function () {
+  const foo = 'bar';
+  const store = {
+    items: [{
+        id: cuid(),
+        name: 'apples',
+        checked: false
+      },
+      {
+        id: cuid(),
+        name: 'oranges',
+        checked: false
+      },
+      {
+        id: cuid(),
+        name: 'milk',
+        checked: true
+      },
+      {
+        id: cuid(),
+        name: 'bread',
+        checked: false
+      }
+    ],
+    hideCheckedItems: false,
+    searchTerm: ''
+  };
+
+  // console.log(store.hideCheckedItems);
+  
+  return {
+    items: store.items,
+    hideCheckedItems: store.hideCheckedItems,
+    searchTerm: store.searchTerm
+  }
+}());
+
+// console.log(Stores);
+// console.log(foo);
